28ec927a773eebb7279ab073510e58eb132efefa
houlasovansela18 <sela.3meng@gmail.com>
2024-06-19 16:31:56 +0700

    add react navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import {
-  Button,
-  NativeModules,
-  SafeAreaView,
-  useColorScheme,
-} from 'react-native';
-import useAppStyle from './src/styles/app-style';
-import { __debug__, __appname__ } from './src/configs/app-utils';
 
-const { SunmiModule } = NativeModules;
+import { __debug__, __appname__ } from './src/configs/app-utils';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useColorScheme } from 'react-native';
+
+import HomeScreen from './src/screens/home';
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: 'Home',
+  screenOptions: {
+    headerStyle: { backgroundColor: 'black' },
+    headerTitleStyle: { color: 'white' },
+  },
+  screens: {
+    Home: {
+      screen: HomeScreen,
+    },
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
 
 function App(): React.JSX.Element {
   const sysColorScheme = useColorScheme();
@@ -16,31 +28,7 @@ function App(): React.JSX.Element {
     console.debug('application-name:', __appname__);
     console.debug('color-scheme:', sysColorScheme);
   }
-  const style = useAppStyle();
-  const handleOnPressed = () => {
-    SunmiModule.initPrinter();
-    SunmiModule.printBitmap(
-      'https://chatgpt.com/images/chatgpt-icon-144x144.png',
-    );
-  };
-  return (
-    <SafeAreaView
-      style={[
-        style.flex1,
-        style.displayFlex,
-        style.backgroundColorPrimary,
-        style.paddingVertical8,
-        style.alignItemsCenter,
-        style.justifyContentCenter,
-      ]}>
-      <Button
-        title="Get device model"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-        onPress={handleOnPressed}
-      />
-    </SafeAreaView>
-  );
+  return <Navigation />;
 }
 
 export default App;
