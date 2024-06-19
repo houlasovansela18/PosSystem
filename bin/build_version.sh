8cd520b688e840083ccdf04bdb2882d1a785ccc6
houlasovansela18 <sela.3meng@gmail.com>
2024-06-19 09:18:37 +0700

    add package.json versions cli controller

--- bin/build_version.sh
+++ bin/build_version.sh
@@ -7,5 +7,5 @@ if [ -z "$NODE_PATH" ]; then
   echo "Node.js not found in PATH."
   exit 1
 fi
-
-$NODE_PATH ../semver.js "$@"
+# the string "#@" is used to parse comment from shell script to node cli.
+$NODE_PATH semver.js "$@"
