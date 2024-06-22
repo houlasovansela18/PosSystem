a26ad66db741ea2ac7d9f6cb0a28b72303e6fa7a
houlasovansela18 <sela.3meng@gmail.com>
2024-06-22 15:55:58 +0700

    add navigation system

--- App.tsx
+++ App.tsx
@@ -1,34 +1,19 @@
 import React from 'react';
 
 import { __debug__, __appname__ } from './src/configs/app-utils';
-import { createStaticNavigation } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 import { useColorScheme } from 'react-native';
+import RootStack from './src/navigator/root-stack';
 
-import HomeScreen from './src/screens/home';
-
-const RootStack = createNativeStackNavigator({
-  initialRouteName: 'Home',
-  screenOptions: {
-    headerStyle: { backgroundColor: 'black' },
-    headerTitleStyle: { color: 'white' },
-  },
-  screens: {
-    Home: {
-      screen: HomeScreen,
-    },
-  },
-});
-
-const Navigation = createStaticNavigation(RootStack);
-
-function App(): React.JSX.Element {
+export default function App(): React.JSX.Element {
   const sysColorScheme = useColorScheme();
   if (__debug__ || __DEV__) {
     console.debug('application-name:', __appname__);
     console.debug('color-scheme:', sysColorScheme);
   }
-  return <Navigation />;
+  return (
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+  );
 }
-
-export default App;
